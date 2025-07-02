b188fd83353797c831b66acf3e2ffc2668857cf0
Dawn Schumacher <dawn@berkeley.edu>
2025-07-02 13:16:40 -0700

    Finalize gitbug script demo

---
+++ README.md
@@ -0,0 +1,4 @@
+# cs61b-cli
+
+This repository contains various command line tools I've created to ease tasks on CS 61B course staff.
+Installation guides soon
--- src/gitbug/gitbug.py
+++ src/gitbug/gitbug.py
@@ -1,7 +1,11 @@
 import os
+import stat
 import json
 import requests
 import hashlib
+from subprocess import run
+from shutil import move, rmtree
+from re import findall
 from argparse import ArgumentParser
 
 gitbug_folder = os.path.expanduser("~/.gitbug")
@@ -9,15 +13,16 @@ coursespec_path = os.path.expanduser("~/.gitbug/coursespec.json")
 coursespec_url = "https://raw.githubusercontent.com/chemicaldawn/cs61b-cli/refs/heads/main/resources/coursespec.json"
 
 def parse_args():
-    parser = ArgumentParser(
+    parser : ArgumentParser = ArgumentParser(
         prog = "gitbug",
         description = "",
     )
     parser.add_argument("<repo-id>")
     parser.add_argument("<assignment-id>")
     parser.add_argument("output-path", nargs="?", default="./")
-    parser.add_argument("-u")
-    return parser.parse_args()
+    parser.add_argument("-u","--update", action="store_true")
+    args = parser.parse_args()
+    return args
 
 def check_file_structure():
     if not os.path.exists(gitbug_folder):
@@ -60,12 +65,57 @@ def clone_down(repo_id : str, assignment_id: str, output_path: str, coursespec):
         exit()
 
     os.mkdir(clone_location)
-    print(coursespec)
-    
+
+    student_repo_ssh = coursespec["student-repo-ssh"].replace("%",repo_id)
+    course_repo_ssh = coursespec["course-repo-ssh"]
+
+    os.chdir(clone_location)
+
+    # set up student files
+    run(["git","init"])
+    sparse_checkout_location = ".git/info/sparse_checkout"
+
+    run(["git","remote","add","student",student_repo_ssh])
+
+    run(["git","sparse-checkout","init"])
+    run(["git","sparse-checkout","set",f"{assignment_id}/"])
+
+    run(["git","pull","student","main"]) 
+
+    move(f"{assignment_id}/tests/",f"{assignment_id}/tests_student/")
+
+    # set up grader files
+    containing_folder = findall(r"([a-z]+)[0-9]", assignment_id)[0]
+    tests_location = f"{containing_folder}/{assignment_id}/grader/submit/"
+
+    run(["git","clone","--no-checkout",course_repo_ssh,".materials"])
+    os.chdir(".materials")
+
+    run(["git","sparse-checkout","init"])
+    run(["git","sparse-checkout","set",f"{tests_location}/"])
+
+    run(["git","checkout","main"])
+    os.chdir("..")
+
+    move(os.path.join(".materials",tests_location), f"{assignment_id}/tests_grader")
+
+    # cleanup
+    print("Cleaning up...")
+    for root, dirs, files in os.walk(".materials", topdown=False):
+        for name in files:
+            filename = os.path.join(root, name)
+            os.chmod(filename, stat.S_IWRITE)
+            os.remove(filename)
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    os.rmdir(".materials")
+    print("Success! Happy debugging :)")
 
 def main():
     args = vars(parse_args())
     check_file_structure()
+    if (args["update"]):
+        update(False)
     coursespec = get_coursespec()
     clone_down(args["<repo-id>"], args["<assignment-id>"], args["output-path"], coursespec)
 
